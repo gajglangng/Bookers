29001d028bd22b8b8e4c97b4c1d8219f18efd976
EC2 Default User <ec2-user@ip-172-31-2-245.ap-northeast-1.compute.internal>
2022-08-11 13:58:19 +0000

    validation

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,25 @@
 class BooksController < ApplicationController
-  
+
   def index
     @book = Book.new # post new book
     @books = Book.all # get all book
-    
+
   end
-  
+
   def show
-    @book = Book.find(params[:id]) 
+    @book = Book.find(params[:id])
+  end
+
+  
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book.id), notice: 'Book was successfully createed.'
+    else
+      @books = Book.all  # get all book 
+      render :index
+    end
   end
   
   def edit
@@ -16,29 +28,25 @@ class BooksController < ApplicationController
 
   def update
     book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path
-  end  
-
-  def create
-    @book = Book.new(book_params)
-    if @book.save
-      redirect_to book_path(@book)
+    if @book.update(book_params)
+    redirect_to book_path(@book), notice: 'Book was successfully updated.'
     else
-      @books = Book.all  # get all book
-      render :index
-  end  
-  
-  def destroy
-    book = book.find(params[:id])  # データ（レコード）を1件取得
-    book.destroy  # データ（レコード）を削除
-    redirect_to books_path  # 投稿一覧画面へリダイレクト  
+     @books = Book.all
+     render :edit
+    end 
   end
-  
-end  
-  
+
+  def destroy
+    book = Book.find(params[:id])  # データ（レコード）を1件取得
+    book.destroy  # データ（レコード）を削除
+    redirect_to books_path, notice: 'Book was successfully destroyed.'
+    # 投稿一覧画面へリダイレクト
+  end
+
+
+
   private
-  
+
   def book_params
     params.require(:book).permit(:title, :body)
   end
